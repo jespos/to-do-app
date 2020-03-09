0c1cf25035e48b5eab0f51561fa8e3b78473cccb
Jolene Esposito <jolene.art@gmail.com>
2020-03-09 13:36:58 -0400

    create app.js. update index.html

---
+++ app.js
@@ -0,0 +1,39 @@
+function onReady() {
+  const addToDoForm = document.getElementById('addToDoForm');
+  const newToDoText = document.getElementById('newToDoText');
+  const toDoList = document.getElementById('toDoList');
+
+  addToDoForm.addEventListener('submit', event => {
+    event.preventDefault();
+
+    // get the text //
+    let title = newToDoText.value;
+
+    // create a new li //
+    let newLi = document.createElement('li');
+
+    //create a new input //
+    let checkbox = document.createElement('input');
+
+    // set the input's type to checkbox //
+    checkbox.type = "checkbox";
+
+    // set the title //
+    newLi.textContent = title;
+
+    //attach the checkbox to the li //
+    newLi.appendChild(checkbox);
+
+    //attach the li to the ul //
+    toDoList.appendChild(newLi);
+
+    //remove the text from the input, so the user doesn't have to delete what they previously entered //
+    newToDoText.value = '';
+  });
+}
+
+
+window.onload = function() {
+  alert('The window has loaded!');
+  onReady();
+};
