9bca7fbb31759274bf37daaee1d1ea2d2878a478
Jolene Esposito <jolene.art@gmail.com>
2020-03-10 22:08:31 -0400

    dom scripting part 2

--- app.js
+++ app.js
@@ -1,39 +1,48 @@
 function onReady() {
-  const addToDoForm = document.getElementById('addToDoForm');
-  const newToDoText = document.getElementById('newToDoText');
-  const toDoList = document.getElementById('toDoList');
+  let toDos = []; //emtpy array of the to-dos that will be entered
+  const addToDoForm = document.getElementById('addToDoForm'); // writing this lets us access the html FORM
 
-  addToDoForm.addEventListener('submit', event => {
-    event.preventDefault();
 
-    // get the text //
-    let title = newToDoText.value;
+  function createNewToDo() { //creating a new to-do, changes the state. This function will update our array of to-dos
+    const newToDoText = document.getElementById('newToDoText'); // this gets us the text (or 'title') of what the user types in
+    if (!newToDoText.value) { return; }
+    toDos.push( {                  //we are adding the todo to the array
+      title: newToDoText.value,   // this gives us the text value of newToDoText
+      complete: false            // this means the box hasn't been checked b/c they just added it to the list
+    });
+    newToDoText.value = ''; // this clears the text input so the user can enter a new to-do
 
-    // create a new li //
-    let newLi = document.createElement('li');
+    renderTheUI(); // call renderTheUI() each time the state changes. So far, the only time the state changes is when we add a new to-do using the createNewToDo() function
+  };
 
-    //create a new input //
-    let checkbox = document.createElement('input');
+  function renderTheUI() {
+    const toDoList = document.getElementById('toDoList'); //access the <ul>
 
-    // set the input's type to checkbox //
-    checkbox.type = "checkbox";
+    toDoList.textContent = ''; //each time we call the renderTheUI() function, it will add new lis to the ul. So before the forEach() function, we need to set newLi to an empty string.
 
-    // set the title //
-    newLi.textContent = title;
+    toDos.forEach(function(toDo) { //forEach takes a function and applies it to each item in the array - in this case our array is toDo. This is how we render each to-do in the array as a li in the ul
+      const newLi = document.createElement('li'); // create a new <li>
+      const checkbox = document.createElement('input'); // create a new input
+      checkbox.type = "checkbox"; //set the input's type to 'checkbox'
 
-    //attach the checkbox to the li //
-    newLi.appendChild(checkbox);
+      newLi.textContent = toDo.title;
 
-    //attach the li to the ul //
-    toDoList.appendChild(newLi);
+      toDoList.appendChild(newLi); //add the new <li> to the <ul>
+      newLi.appendChild(checkbox); //add the checkbox to the <li>
 
-    //remove the text from the input, so the user doesn't have to delete what they previously entered //
-    newToDoText.value = '';
+    });
+  };
+
+
+  addToDoForm.addEventListener('submit', event => { // adding an event listener to our form
+    event.preventDefault(); // prevents the page from reloading after submitting your to do
+    createNewToDo(); //calls the createNewToDo function we created above
   });
-}
+
+  renderTheUI(); //renders UI based on the current state. When the app first loads the state will be an empty input box and button
+};
 
 
 window.onload = function() {
-  alert('The window has loaded!');
   onReady();
 };
